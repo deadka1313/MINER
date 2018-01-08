1d82897b6192661f1925cef97e4ed493802f9e62
deadka1313 <deadka1313@gmail.com>
2018-01-08 17:41:34 +0700

    Немного больше каркаса. Иметод получения изображения. И класс перечисления для картинок(Я научился создавать jar файл для запуска)

--- src/JavaSweeper.java
+++ src/JavaSweeper.java
@@ -1,3 +1,5 @@
+import sweeper.Box;
+
 import javax.swing.*;
 import java.awt.*;
 
@@ -12,15 +14,24 @@ public class JavaSweeper extends JFrame {
     }
 
     private JavaSweeper() {
+        setImages();
         initPanel();
         initFrame();
     }
 
     private void initPanel() {
-        panel = new JPanel();
+        panel = new JPanel() {
+            //Рисует все
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                for (Box box : Box.values())
+                    g.drawImage((Image)box.image, box.ordinal() * IMAGE_SIZE, 0, this);
+            }
+        };
         panel.setPreferredSize(new Dimension(
                 COLS * IMAGE_SIZE, ROWS * IMAGE_SIZE));
-        add (panel);
+        add(panel);
     }
 
     private void initFrame() {
@@ -30,5 +41,17 @@ public class JavaSweeper extends JFrame {
         setLocationRelativeTo(null);
         setResizable(false);
         setVisible(true);
+        setIconImage(getImage("icon"));
+    }
+
+    private void setImages() {
+        for (Box box : Box.values())
+            box.image = getImage(box.name());
+    }
+
+    private Image getImage(String name) {
+        String filename = "img/" + name.toLowerCase() + ".png";
+        ImageIcon icon = new ImageIcon(getClass().getResource(filename));
+        return icon.getImage();
     }
 }
